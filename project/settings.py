3e7d4b865adef3a185ce806e6d7c93f67625b8a5
heawer <accounts+ghtest@sapphic.moe>
2025-03-07 10:53:35 +0500

    fix

--- project/settings.py
+++ project/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u5zpst=l#c_n150g%4yd$jz2@7)oqv=en&1r9#5&bq=4i=nmnu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
